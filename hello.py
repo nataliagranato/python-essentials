eff91abe12043e3b425e354b0591356bef6868fa
nataliagranato <natalia.granato@jackexperts.com>
2024-01-11 12:52:55 -0300

    Adicionando SHEBANG e comentários.

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
-print('Hello, World!')
-print('nataliagranato' .upper())
+#!/usr/bin/env python3
+# Imprime 'Olá, mundo!' e a versão maiúscula de 'nataliagranato'.
+print('Hello, World!')  # test-ignore
+print('nataliagranato' .upper())  # test-ignore
