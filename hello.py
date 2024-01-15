732180c8d00c010e94ea1e6b63d691c74b38aa90
nataliagranato <natalia.granato@jackexperts.com>
2024-01-15 11:57:30 -0300

    Implementação do hello world multi linguagem.

--- hello.py
+++ hello.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
-# Imprime 'Olá, mundo!' e a versão maiúscula de 'nataliagranato'.
-print('Hello, World!')  # test-ignore
-print('nataliagranato' .upper())  # test-ignore
+"""Hello World multi languages.
+Depending on the language configured in the environment, the program displays the message
+corresponding.
+
+Usage:
+
+Have the LANG variable properly configured in the environment, e.g.:
+    export LANG=pt_BR.UTF-8
+    ou export LANG=us_EN.UTF-8
+    
+Run the program:
+     python3 variables.py
+"""
+
+__version__ = '0.0.1'
+__author__ = 'Natália Granato'
+__license__ = 'unlicense'
+
+import os  # import the os module
+
+# get the LANG variable from the environment
+current_language = os.getenv("LANG", "pt_BR.UTF-8")[:5]
+
+msg = "Olá, Mundo!"
+
+if current_language == "us_EN.UTF-8":
+    msg = "Hello, World!"
+
+elif current_language == "es_ES.UTF-8":
+    msg = "¡Hola Mundo!"
+
+elif current_language == "fr_FR.UTF-8":
+    msg = "Bonjour le monde!"
+
+print(msg)
